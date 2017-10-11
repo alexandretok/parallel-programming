70eddf8ac35a51b54a098eda40d3c5d84bff50ad
Alexandre Justino <alexandretok@gmail.com>
2017-10-11 08:25:09 -0300

    Add random scalar and get input from user

--- scalar-product.c
+++ scalar-product.c
@@ -3,24 +3,19 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-void Get_input(int my_rank, int comm_sz, int* a, int* b, int n);
-void Generate_list(int my_rank, int comm_sz, int* a, int* b, int n);
+void Get_input(int my_rank, int comm_sz, int* a, int* b, int n, int escalar);
+void Generate_list(int my_rank, int comm_sz, int* a, int* b, int n, int escalar);
 
 int main(void) {
    int my_rank, comm_sz, n;
    int *a, *b, escalar, local_total = 0, global_total = 0;
-   double time;
+   double tempo, tempo_total;
 
    MPI_Init(NULL, NULL);
    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 
-   char option;
-
    if(!my_rank){
-      printf("Digite um escalar:\n");
-      scanf(" %d", &escalar);
-
       printf("Qual a dimensão dos vetores:\n");
       scanf("%d", &n);
       if(n % comm_sz != 0){
@@ -28,41 +23,37 @@ int main(void) {
          // exit(1);
          MPI_Abort(MPI_COMM_WORLD, 0);
       }
+   }
 
-      printf("Digitar (d) ou gerar (g) vetores?\n");
-      scanf(" %c", &option);
+   if(!my_rank){
+   	srand(time(NULL));
+   	escalar = rand() % 5 + 1;
+   	printf("Escalar gerado: %i\n", escalar);
    }
 
    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
-   MPI_Bcast(&option, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
    MPI_Bcast(&escalar, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
    a = (int*) malloc(n*sizeof(int));
    b = (int*) malloc(n*sizeof(int));
 
-   if(!my_rank)
-      time = MPI_Wtime();
-
-   if(option == 'd')
-      Get_input(my_rank, comm_sz, a, b, n);
-   else Generate_list(my_rank, comm_sz, a, b, n);
-
-   if(!my_rank){
-      printf("Arrays definidos em %.2f segundos!\n", MPI_Wtime() - time);
-      time = MPI_Wtime();
-   }
+   Get_input(my_rank, comm_sz, a, b, n, escalar);
+   
+   tempo = MPI_Wtime();
 
    for(int i = 0; i < n/comm_sz; i++){
+   	  a[i] *= escalar;
+   	  b[i] *= escalar;
       local_total += a[i] * b[i];
    }
 
-   printf("%.2f segundos para produto interno\n", MPI_Wtime() - time);
+   tempo = MPI_Wtime() - tempo;
 
-   // printf("Resposta do processo %i: %i\n", my_rank, local_total);
-
+   MPI_Reduce(&tempo, &tempo_total, 2, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
    MPI_Reduce(&local_total, &global_total, 2, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
    if (!my_rank) {
+      printf("%.2f milisegundos para produto interno\n", tempo * 1000);
       printf("Resposta: %i\n", global_total);
    }
 
@@ -71,35 +62,36 @@ int main(void) {
    return 0;
 } /*  main  */
 
-void Get_input(int my_rank, int comm_sz, int* a, int* b, int n) {
+void Get_input(int my_rank, int comm_sz, int* a, int* b, int n, int escalar) {
    if (my_rank == 0) {
       printf("Digite os elementos do vetor 1:\n");
-      for (int i = 0; i < n; i++)
+      for (int i = 0; i < n; i++){
          scanf("%d", &a[i]);
-      printf("Digite os elementos do vetor 2:\n");
+         a[i] *= escalar;
+      }
+      printf("Multiplicado por escalar: ");
       for (int i = 0; i < n; i++)
+      	printf("%d ", a[i]);
+      printf("\n");
+      printf("Digite os elementos do vetor 2:\n");
+      for (int i = 0; i < n; i++){
          scanf("%d", &b[i]);
+         b[i] *= escalar;
+      }
+      printf("Multiplicado por escalar: ");
+      for (int i = 0; i < n; i++)
+      	printf("%d ", b[i]);
+      printf("\n");
    }
 
    MPI_Scatter(a, n/comm_sz, MPI_INT, a, n/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
    MPI_Scatter(b, n/comm_sz, MPI_INT, b, n/comm_sz, MPI_INT, 0, MPI_COMM_WORLD);
 }  /* Get_input */
 
-void Generate_list(int my_rank, int comm_sz, int* a, int* b, int n) {
+void Generate_list(int my_rank, int comm_sz, int* a, int* b, int n, int escalar) {
       srand(time(NULL) * (my_rank + 1));
       for (int i = 0; i < n / comm_sz; i++){
-         a[i] = rand() % 10;
-         b[i] = rand() % 10;
+         a[i] = rand() % 5 * escalar;
+         b[i] = rand() % 5 * escalar;
       }
-
-      // printf("A: ");
-      // for (int i = 0; i < n; i++){
-      //    printf(" %i", a[i]);
-      //    // a[i] *= escalar;
-      // }
-
-      // printf("\nB: ");
-      // for (int i = 0; i < n; i++){
-      //    printf(" %i", b[i]);
-      // } printf("\n");
 }  /* Generate_list */
